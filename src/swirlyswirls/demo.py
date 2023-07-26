dae100c4eec26f9b67c278877fd53b56217444d2
Michael Lamertz <michael.lamertz@gmail.com>
2023-07-26 23:31:02 +0200

    Demo launcher + pond demo

---
+++ src/swirlyswirls/demo.py
@@ -0,0 +1,45 @@
+import sys
+import os.path
+import argparse
+import pygame
+
+from importlib import import_module
+from importlib.resources import contents
+from types import SimpleNamespace
+
+from pygamehelpers.framework import App
+
+TITLE = 'swirlyswirls Demos'
+SCREEN = pygame.rect.Rect(0, 0, 1024, 768)
+FPS = 60
+
+
+def main():
+    cmdline = argparse.ArgumentParser(description='swirlyswirls demo runner')
+    cmdline.add_argument('demo', type=str, nargs='?', help='Demo name.  Leave out to get list of demos.')
+    opts = cmdline.parse_args(sys.argv[1:])
+
+    if opts.demo is None:
+        print('Available demos:')
+        demos = [os.path.splitext(f)[0] for f in contents('swirlyswirls.demos') if not f.startswith('__')]
+        for demo in demos:
+            print(f'    {demo}')
+        sys.exit(0)
+
+    fullname = f'swirlyswirls.demos.{opts.demo}'
+    imp = import_module(fullname)
+    cls = getattr(imp, 'Demo')
+
+    app = App(TITLE, SCREEN, FPS)
+
+    persist = SimpleNamespace(
+        font=pygame.Font(None),
+    )
+
+    states = { 'demo': cls(app, persist) }
+
+    app.run('demo', states)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/swirlyswirls/demos/pond.py
@@ -0,0 +1,100 @@
+import pygame
+import tinyecs as ecs
+import tinyecs.components as ecsc
+
+from functools import partial
+
+from cooldown import Cooldown
+from pygame import Vector2
+from pygamehelpers.framework import GameState
+
+from swirlyswirls.spritegroup import ReversedGroup
+from swirlyswirls.compsys import (ESprite, TRSA, momentum_system, trsa_system)
+from swirlyswirls.particles import Bubble, bubble_system
+from swirlyswirls.emitter import Emitter, emitter_system
+from swirlyswirls.zones import ZoneCircle
+
+
+class Demo(GameState):
+    def __init__(self, app, persist, parent=None):
+        super().__init__(app, persist, parent=parent)
+
+        self.title = 'Pond Demo'
+        self.cache = {}
+        self.group = ReversedGroup()
+        self.momentum = False
+
+        self.label = self.persist.font.render('Press space to toggle momentum', True, 'white')
+
+        self.setup_ecs()
+
+        self.launch_emitter(self.app.rect.center,
+                            Emitter(
+                                ept0=3, ept1=3, tick=0.05,
+                                zone=ZoneCircle(r0=0, r1=128),
+                                launcher=partial(self.launch_pond_particle,
+                                                 group=self.group, cache=self.cache))
+                            )
+
+    def setup_ecs(self):
+        ecs.add_system(ecsc.lifetime_system, 'lifetime')
+        ecs.add_system(emitter_system, 'emitter', 'trsa', 'lifetime')
+        ecs.add_system(bubble_system, 'bubble', 'sprite', 'trsa', 'lifetime', 'cache')
+        ecs.add_system(trsa_system, 'trsa', 'sprite', 'cache')
+
+    @staticmethod
+    def launch_pond_particle(position, momentum, group, cache):
+        e = ecs.create_entity()
+        ecs.add_component(e, 'bubble', Bubble(r0=5, r1=10,
+                                              # alpha0=0, alpha1=255,
+                                              alpha0=128, alpha1=0,
+                                              base_color='aqua', highlight_color='white'))
+        ecs.add_component(e, 'sprite', ESprite(group))
+        ecs.add_component(e, 'trsa', TRSA(translate=position))
+        ecs.add_component(e, 'momentum', momentum)
+        ecs.add_component(e, 'lifetime', Cooldown(1))
+        ecs.add_component(e, 'cache', cache)
+
+    @staticmethod
+    def launch_emitter(pos, emitter):
+        e = ecs.create_entity('emitter')
+        ecs.add_component(e, 'emitter', emitter)
+        ecs.add_component(e, 'trsa', TRSA(translate=Vector2(pos)))
+        ecs.add_component(e, 'lifetime', Cooldown(5).pause())
+
+    def reset(self, persist=None):
+        """Reset settings when re-running."""
+        super().reset(persist=persist)
+        ...
+
+    def dispatch_event(self, e):
+        """Handle user events"""
+        super().dispatch_event(e)
+
+        match e.type:
+            case pygame.KEYDOWN if e.key == pygame.K_SPACE:
+                self.momentum = not self.momentum
+                if self.momentum:
+                    ecs.add_system(momentum_system, 'momentum', 'trsa')
+                else:
+                    ecs.remove_system(momentum_system)
+
+    def update(self, dt):
+        """Update frame by delta time dt."""
+        ecs.run_all_systems(dt)
+
+        self.group.update(dt)
+
+        sprites = len(self.group.sprites())
+        c = len(list(self.cache.keys()))
+        pygame.display.set_caption(f'{self.title} - time={pygame.time.get_ticks()/1000:.2f}  fps={self.app.clock.get_fps():.2f}  {sprites=}  {c=}')
+
+    def draw(self, screen):
+        """Draw current frame to surface screen."""
+
+        screen.fill('black')
+        screen.blit(self.label, (5, 5))
+
+        self.group.draw(screen)
+
+        pygame.display.flip()
