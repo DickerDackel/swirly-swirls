e0996104c0d44c1e96a82a0711df3642b98d8514
Michael Lamertz <michael.lamertz@gmail.com>
2023-07-29 20:26:11 +0200

    Added single point zone.

--- src/swirlyswirls/__init__.py
+++ src/swirlyswirls/__init__.py
@@ -3,4 +3,4 @@ from .spritegroup import ReversedGroup
 from .compsys import ESprite, TRSA
 from .emitter import Emitter, emitter_system
 from .particles import Bubble, bubble_system
-from .zones import ZoneBeam, ZoneCircle, ZoneLine, ZoneRect
+from .zones import ZonePoint, ZoneBeam, ZoneCircle, ZoneLine, ZoneRect
