74fc06adf8ade0f10f81ca3e07015ad4d35dd24e
Michael Lamertz <michael.lamertz@gmail.com>
2023-08-08 01:12:51 +0200

    Refactored Particle by adding LerpThing to tinyecs

--- src/swirlyswirls/emitter.py
+++ src/swirlyswirls/emitter.py
@@ -132,6 +132,7 @@ run `swirlyswirl-demo`, and inspect the `swirlyswirls.bubbles` module.
 """
 
 import tinyecs as ecs
+import tinyecs.compsys as ecsc
 import swirlyswirls.zones
 
 from dataclasses import dataclass, InitVar
@@ -323,82 +324,21 @@ class Particle:
 
     Parameters
     ----------
-    size_min, size_max: float = 2, 32
-        initial and final surface size
+    rotate: LerpThing
+        Management of rotation
 
-    size_ease: callable = lambda x: x
-        An easing function to put over the interpolation of size_min and size_max
+    scale: LerpThing
+        Management of scaling
 
-    alpha_min, alpha_max: float = 255, 0
-        Initial and final alpha of the image
-
-    alpha_ease : callable = lambda x: x
-        An easing function to put over the interpolation of alpha_min and alpha_max
-
-    cycle: bool = False
-        Should the particle repeat or end its transmogrification
-
-    image_factory : callable = swirlyswirls.particles._bubble_default_image_factory
-        A default drawing function for the Bubble component
-
-    Attributes
-    ----------
-    All parameters are also accessible as attributes.
-
-    size: float
-        The lerped size between size_min and size_max for the time t.  Used by the
-        `bubble_system`.
-
-    alpha: float
-        The lerped alpha between alpha_min and alpha_max for the time t.  Used by the
-        `bubble_system`.
+    alpha: LerpThing
+        Management of alpha
 
     """
-    size_min: float = 2
-    size_max: float = 32
-    size_ease: callable = lambda x: x
-    alpha_min: float = 255
-    alpha_max: float = 255
-    alpha_ease: callable = lambda x: x
-    cycle: bool = False
-    image_factory : callable = default_image_factory
-
-    def __post_init__(self):
-        self.alpha = self.alpha_min
-        self.size = self.size_min
-
-    @property
-    def image(self):
-        return self.image_factory(size=self.size, alpha=self.alpha)
+    rotate: ecsc.LerpThing
+    scale: ecsc.LerpThing
+    alpha: ecsc.LerpThing
 
 
-def particle_system(dt, eid, particle, lifetime):
-    """Progress size and alpha over lifetime.
-
-    The `Particle` class configures the dynamicall generation of an `image`
-    property to use as a particle with changing size.  The particle_system is
-    the functional part for that component.
-
-    Parameters
-    ----------
-    particle: swirlyswirls.particles.Particle
-        Particle data for the system.
-
-    lifetime : Cooldown
-        Used for both, the size and alpha ramp.
-
-        The removal of the entity at the end of `lifetime` should be handled by
-        tinyecs.components.lifetime_system.  It's not the scope of this system.
-
-    Returns
-    -------
-    None
-
-    """
-    if lifetime.cold and particle.cycle:
-        lifetime.reset()
-
-    t = lifetime.normalized
-
-    particle.size = _lerp(particle.size_min, particle.size_max, particle.size_ease(t))
-    particle.alpha = _lerp(particle.alpha_min, particle.alpha_max, particle.alpha_ease(t))
+def particle_system(dt, eid, particle):
+    """This is a nop, all lerp things handle their updates automagically"""
+    pass
