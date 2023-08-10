57500a0cb1c3f651ba0d534ea7349921877da169
Michael Lamertz <michael.lamertz@gmail.com>
2023-08-10 23:36:31 +0200

    Interface cleanups and removed redundant .copy()

--- src/swirlyswirls/utils.py
+++ src/swirlyswirls/utils.py
@@ -7,14 +7,10 @@ here, but might migrate into more specific packages at any time.
 """
 import tinyecs as ecs
 
-from pygame import Vector2
-from pgcooldown import Cooldown
-
 __all__ = ['particle_entity_factory', 'emitter_entity_factory']
 
 
-def emitter_entity_factory(eid=None, *, emitter, position, momentum,
-                           lifetime, **kwargs):
+def emitter_entity_factory(emitter, position, momentum, lifetime, eid=None, **kwargs):
     """A macro to create emitter entities with common components.
 
     Parameters
@@ -48,27 +44,26 @@ def emitter_entity_factory(eid=None, *, emitter, position, momentum,
         eid = ecs.create_entity()
 
     ecs.add_component(eid, 'emitter', emitter)
-    ecs.add_component(eid, 'position', Vector2(position))
-    ecs.add_component(eid, 'momentum', Vector2(momentum))
-    ecs.add_component(eid, 'lifetime', Cooldown(lifetime))
+    ecs.add_component(eid, 'position', position)
+    ecs.add_component(eid, 'momentum', momentum)
+    ecs.add_component(eid, 'lifetime', lifetime)
     for cid, comp in kwargs.items():
         ecs.add_component(eid, cid, comp)
 
     return eid
-    ecs.add_component(e, 'lifetime', Cooldown(5))
 
 
-def particle_entity_factory(eid=None, *, particle, position, momentum,
-                            lifetime, sprite, emitter_eid, **kwargs):
+def particle_entity_factory(particle, position, momentum, lifetime, sprite, emitter_eid, eid=None, **kwargs):
     """A macro to create particle entities with common components.
 
     Parameters
     ----------
-    eid: hashable
-        The eid of the particle to setup.  If None, will be created.
-
     particle: swirlyswirls.Particle
         The particle lifecycle description
+
+    position,
+    momentum: Vector2
+        Location and directional speed of the particle
 
     lifetime: float | int | pgcooldown.Cooldown
         Lifetime of the particle
@@ -78,12 +73,11 @@ def particle_entity_factory(eid=None, *, particle, position, momentum,
         swirlyswirls.EVSprite to dynamically generate images from an
         externally plugged in function is recommended.
 
-    position,
-    momentum: Vector2
-        Location and directional speed of the particle
-
-    emitted_by: hashable
+    emitter_eid: hashable
         The EID of the emitter that created this particle.
+
+    eid: hashable
+        The eid of the particle to setup.  If None, will be created.
 
     *kwargs:
         All additional key/value pairs will be directly added as
@@ -101,10 +95,10 @@ def particle_entity_factory(eid=None, *, particle, position, momentum,
         eid = ecs.create_entity()
 
     ecs.add_component(eid, 'particle', particle)
-    ecs.add_component(eid, 'lifetime', Cooldown(lifetime))
+    ecs.add_component(eid, 'position', position)
+    ecs.add_component(eid, 'momentum', momentum)
+    ecs.add_component(eid, 'lifetime', lifetime)
     ecs.add_component(eid, 'sprite', sprite)
-    ecs.add_component(eid, 'position', Vector2(position))
-    ecs.add_component(eid, 'momentum', Vector2(momentum))
     ecs.add_component(eid, 'emitted-by', emitter_eid)
     for cid, comp in kwargs.items():
         ecs.add_component(eid, cid, comp)
